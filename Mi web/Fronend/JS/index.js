d08ec668d14a27b07a9ecbb0c6bddb71d7f41263
Ruben-CT <colmeneroby9@gmail.com>
2024-06-21 15:15:52 -0600

    Add files via upload

---
+++ Mi web/Fronend/JS/index.js
@@ -0,0 +1,11 @@
+var colores = ['rojo','azul','verde'];
+console.log(colores);
+colores.push('negre','blanco');
+console.log(colores);
+//Interacción con HTML
+var opciones = '';
+var tabla='';
+for (var i=0;< colores.length;i++){
+    opciones += '<option value="' + i +
+    
+}
---
+++ Mi web/Fronend/JS/mapa.js
@@ -0,0 +1,14 @@
+function iniciarMap(){
+
+    var coord = { lat:21.17000 ,Ing: -100.93000};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+     zoom:10,
+    center: coord
+  });
+  var marker = new google.maps.Marker({
+
+    position: coord,
+    map: map
+  });
+}
